34ead5ecd4ef6edc8783eb88a22865c386ecb573
Parmeet Singh <psparmeet14@gmail.com>
2026-05-30 10:46:54 +0530

    refactor: remove redundant public modifier for main method

--- src/main/java/com/psb/potential/PotentialApplication.java
+++ src/main/java/com/psb/potential/PotentialApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PotentialApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(PotentialApplication.class, args);
 	}
 
